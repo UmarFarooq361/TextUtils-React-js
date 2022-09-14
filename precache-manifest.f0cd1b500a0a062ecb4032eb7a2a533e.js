eb1c02e8dbb4998aff488fc6b6282001e90ae145
umar farooq <umar@gmail.com>
2022-09-14 11:34:42 +0500

    Updates

---
+++ precache-manifest.f0cd1b500a0a062ecb4032eb7a2a533e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7e4f3638bc2324b5308",
+    "url": "/TextUtils-React-js/static/css/main.247e88ae.chunk.css"
+  },
+  {
+    "revision": "e7e4f3638bc2324b5308",
+    "url": "/TextUtils-React-js/static/js/main.e7e4f363.chunk.js"
+  },
+  {
+    "revision": "2998caf54286a93ccb62",
+    "url": "/TextUtils-React-js/static/js/1.2998caf5.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/TextUtils-React-js/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "92d4936cfbd31c5ee06c",
+    "url": "/TextUtils-React-js/static/js/runtime~main.92d4936c.js"
+  },
+  {
+    "revision": "037f210422b6d9e60e039c9aac5a54a3",
+    "url": "/TextUtils-React-js/index.html"
+  }
+];
